09933e81b7913b8758c934074b037bb38b56eb72
Ed Shadi <edshadi@gmail.com>
2014-02-03 15:58:52 -0800

    multiple ways to pass and call a block

--- week-2/discussions/blocks/iterator_r5.rb
+++ week-2/discussions/blocks/iterator_r5.rb
@@ -1,4 +1,28 @@
 # r5: ok, we can't come to you everytime we want to add an operation, can you please build us an iterator that supports any number of operations?
+def iterator array
+  index = 0
+  while index < array.length
+    yield array[index]
+    index += 1
+  end
+end
+
+iterator [1, 2, 3] do |num|
+  puts num * num
+end
+
+def iterator array, &operation
+  index = 0
+  while index < array.length
+    operation.call array[index]
+    index += 1
+  end
+end
+
+iterator [1, 2, 3] do |num|
+  puts num * num
+end
+
 def iterator array, block1, block2
   index = 0
   while index < array.length
